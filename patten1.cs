91f204d82f61efcadfa6d89a20d26f62c3424466
blessonsaju <58213411+blessonsaju@users.noreply.github.com>
2022-12-09 10:35:14 +0530

    Add files via upload

---
+++ patten1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern
+{
+    class patten1
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++) //num of row
+            {
+                for(int j=1;j<=(5-i);j++)   //num of space -decrement     
+                {
+                    Console.Write(" ");
+
+                }
+                for(int k =1;k<=i;k++)  // LEFT *printing
+                {
+                    Console.Write("*");
+                }
+                for(int l=(i-1);l>0;l--)    //PRINTING RIGHT SIDE
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+           
+          
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pattern2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern
+{
+    class pattern2
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=6;i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();  
+        }
+        
+    }
+}
